36f52a9385593f3a6341a01c7d590c137ba26b2b
Christian Bergschneider <christian.bergschneider@gmx.de>
2026-08-15 23:59:00 +0200

    feat: update Kotlin client to use CoroutineScope and bump version to 0.1.1

--- ftswarm-kotlin-client/src/io/github/elektrofuzzis/ftswarm_clients/kotlin/FtSwarmClient.kt
+++ ftswarm-kotlin-client/src/io/github/elektrofuzzis/ftswarm_clients/kotlin/FtSwarmClient.kt
@@ -3,14 +3,15 @@ package io.github.elektrofuzzis.ftswarm_clients.kotlin
 import io.github.elektrofuzzis.ftswarm_clients.kotlin.transport.FtSwarmSerialTransport
 import io.github.elektrofuzzis.ftswarm_clients.kotlin.transport.FtSwarmTransport
 import io.github.elektrofuzzis.ftswarm_clients.kotlin.transport.SerialPortIdentifier
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.currentCoroutineContext
 import java.io.Closeable
 import kotlin.coroutines.CoroutineContext
 
-suspend fun FtSwarmClient(port: SerialPortIdentifier, parent: CoroutineContext?): FtSwarmClient {
-    val parentContext = parent ?: currentCoroutineContext()
+suspend fun FtSwarmClient(port: SerialPortIdentifier, parent: CoroutineScope? = null): FtSwarmClient {
+    val parentContext = parent?.coroutineContext ?: currentCoroutineContext()
     val job = SupervisorJob(parentContext[Job])
 
     return FtSwarmClient(
